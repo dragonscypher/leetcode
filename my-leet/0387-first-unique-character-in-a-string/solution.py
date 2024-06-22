474aca4cb36b4618a8394b1e46cab083b1316d10
Shivam Pal <56273423+dragonscypher@users.noreply.github.com>
2024-06-22 08:57:25 +0000

    [SP's LeetCode] Runtime - 73 ms (34.52%), Memory - 17.1 MB (43.01%)

---
+++ my-leet/0387-first-unique-character-in-a-string/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        for key, val in Counter(s).items():
+            if val == 1:
+                return s.index(key)
+        return -1
