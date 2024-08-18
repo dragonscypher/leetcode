1a0ad3477a967fe4ecdb7f846c7ca94ec050726c
Shivam Pal <56273423+dragonscypher@users.noreply.github.com>
2024-08-18 23:36:34 +0000

    [SP's LeetCode] Runtime - 473 ms (5.11%), Memory - 28.3 MB (93.54%)

--- my-leet/0217-contains-duplicate/solution.py
+++ my-leet/0217-contains-duplicate/solution.py
@@ -1,10 +1,7 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        myset = set()
-
-        for n in nums:
-            if n in myset:
+        nums.sort()
+        for i in range(1,len(nums)) :
+            if nums[i] == nums [i-1]:
                 return True
-            myset.add(n)
         return False
-
