50b555715cd7990997b122007f9de9e81a7a9659
Shivam Pal <56273423+dragonscypher@users.noreply.github.com>
2024-10-08 15:44:15 +0000

    [SP's LeetCode] Runtime - 378 ms (56.41%), Memory - 0B (100.00%)

--- my-leet/0570-managers-with-at-least-5-direct-reports/solution.sql
+++ my-leet/0570-managers-with-at-least-5-direct-reports/solution.sql
@@ -1,7 +1,1 @@
-select Name
-from Employee as t1 
-join (select ManagerId
-    from Employee
-    group by ManagerId
-    having count(ManagerId) >= 5) as t2
-on t1.Id = t2.ManagerId
+select name from Employee where id in (select managerId from Employee group by managerId having count(managerId)>4);
