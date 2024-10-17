f4999c95aa6e3040461c9a5aca94565669428840
Shivam Pal <56273423+dragonscypher@users.noreply.github.com>
2024-10-17 22:06:57 +0000

    [SP's LeetCode] Runtime - 15 ms (48.85%), Memory - 17.5 MB (12.86%)

--- my-leet/0242-valid-anagram/solution.py
+++ my-leet/0242-valid-anagram/solution.py
@@ -1,12 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+        c=0
+        if len(s)==len(t) and sorted(s) == sorted(t):
+            return True
+        else:
             return False
-
-        countS, countT = {}, {}
-
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-        return countS == countT
 
