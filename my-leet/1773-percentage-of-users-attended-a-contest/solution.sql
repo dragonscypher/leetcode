1891eaef84e5237ac5aa9c5774dd44cec45827e4
Shivam Pal <56273423+dragonscypher@users.noreply.github.com>
2024-12-01 11:17:21 +0000

    [SP's LeetCode] Runtime - 1080 ms (52.72%), Memory - 0B (100.00%)

---
+++ my-leet/1773-percentage-of-users-attended-a-contest/solution.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select r.contest_id , round((count( r.user_id)/(select count(u.user_id) from Users u)*100),2) as percentage from Register r  
+group by r.contest_id
+order by percentage desc, contest_id ;
