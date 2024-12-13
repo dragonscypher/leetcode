1d80d03081867f1ec1a0e3a34e30dcb7b0535de3
Shivam Pal <56273423+dragonscypher@users.noreply.github.com>
2024-12-13 06:11:45 +0000

    [SP's LeetCode] Runtime - 4 ms (62.67%), Memory - 17.3 MB (22.10%)

--- my-leet/2692-take-gifts-from-the-richest-pile/solution.py
+++ my-leet/2692-take-gifts-from-the-richest-pile/solution.py
@@ -1,11 +1,14 @@
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
-        gifts=[-x for x in gifts]
-        heapify(gifts)
-        x, i=1<<32, 0
-        while i<k and x>1:
-            x=-heappop(gifts)
-            heappush(gifts, -isqrt(x))
-            i+=1
-        return -sum(gifts)
+        nums = [-num for num in gifts]
+        heapify(nums)
+        
+        while k:
+            tmp = math.isqrt(-heappop(nums))
+            heappush(nums, -tmp)
+            k -= 1
+            
+        return -sum(nums)
+        
        
+        
