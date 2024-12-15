1ab1c5b56d191ea6d139a29d908c688a286b73a1
Shivam Pal <56273423+dragonscypher@users.noreply.github.com>
2024-12-15 20:11:23 +0000

    [SP's LeetCode] Runtime - 1759 ms (16.77%), Memory - 17.8 MB (60.67%)

--- my-leet/0001-two-sum/solution.py
+++ my-leet/0001-two-sum/solution.py
@@ -1,10 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = defaultdict(int)
-        for idx, i in enumerate(nums):
-            diff = target - i
-            if diff in d:
-                return [idx, d[diff]]
-            d[i] = idx
-        return []
-
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j] == target:
+                    return (i,j)
