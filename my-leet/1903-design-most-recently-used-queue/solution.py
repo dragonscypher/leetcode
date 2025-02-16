ed442f135cc5dde6b5a3db6e03d490c249ab70ef
Shivam Pal <56273423+dragonscypher@users.noreply.github.com>
2025-02-16 23:32:43 +0000

    [SP's LeetCode] Runtime - 50 ms (66.47%), Memory - 19.3 MB (28.88%)

---
+++ my-leet/1903-design-most-recently-used-queue/solution.py
@@ -0,0 +1,12 @@
+from sortedcontainers import SortedList
+
+class MRUQueue:
+
+    def __init__(self, n: int):
+        self.data = SortedList((i, i) for i in range(1, n+1))
+
+    def fetch(self, k: int) -> int:
+        _, x = self.data.pop(k-1)
+        i = self.data[-1][0] + 1 if self.data else 0
+        self.data.add((i, x))
+        return x
